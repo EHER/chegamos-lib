9baca28f225546d2f772c03ff9660eefbed6e432
Eher <alexandre.eher@apontador.com>
2011-12-12 18:03:02 -0200

    Adicionada as tasks para detectar problemas no código (phpmd). Criação de uma pasta vendor para dependencias externas.

--- Makefile
+++ Makefile
@@ -5,6 +5,15 @@ help:
 	@echo "test\t\t Roda os teste e gera relatório"
 	@echo "coverage\t Abre o relatório de cobertura"
 
+depends:
+	@echo "Atualizando dependências do projeto..."
+	git submodule update
+	pear config-set auto_discover 1
+	pear install pear.phpunit.de/PHPUnit
+	pear channel-discover pear.phpmd.org
+	pear channel-discover pear.pdepend.org
+	pear install --alldeps phpmd/PHP_PMD
+
 test:
 	@echo "Rodando testes e gerando relatório de cobertura..."
 	phpunit
@@ -12,3 +21,12 @@ test:
 coverage:
 	@echo "Abrindo relatório de cobertura de código..."
 	open reports/coverage/index.html
+
+md:
+	@echo "Procurando coisas estranhas no código..."
+	phpmd src/ html codesize,unusedcode,design --reportfile reports/mess/index.html
+
+showmess:
+	@echo "Abrindo relatório de coisas estranhas..."
+	open reports/mess/index.html
+
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,6 +2,7 @@
 
 $pear_path = trim(`pear config-get php_dir`);
 set_include_path('src' 
+        . PATH_SEPARATOR . 'vendor'
         . PATH_SEPARATOR . $pear_path 
         . PATH_SEPARATOR . get_include_path());
 
@@ -13,15 +14,18 @@ spl_autoload_register(
     function($className) {
         $fileParts = explode('\\', ltrim($className, '\\'));
 
-        if (false !== strpos(end($fileParts), '_'))
+        if (false !== strpos(end($fileParts), '_')) {
             array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
+        }
 
         $file = implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
 
         foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-            if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file))
-                return require $path;
+            $path = $path . DIRECTORY_SEPARATOR . $file;
+            if (file_exists($path)) {
+                $success = require($path);
+                return $success;
+            }
         }
     }
 );
-
