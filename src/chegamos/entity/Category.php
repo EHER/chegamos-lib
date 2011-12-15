4e515879198862a9e4496164dbccfb8495967474
Eher <alexandre.eher@apontador.com>
2011-12-15 17:34:56 -0200

    Formatação para atender aos padrões da PEAR

--- src/chegamos/entity/Category.php
+++ src/chegamos/entity/Category.php
@@ -4,56 +4,85 @@ namespace chegamos\entity;
 
 use chegamos\util\Inflector;
 
-class Category {
+/**
+ * @package cheamos\entity
+ */ 
+class Category 
+ { 
+    private $id = "";
+    private $name = "";
+    private $subcategory = "";
 
-	var $id = "";
-	var $name = "";
-	var $subcategory = "";
+    /**
+     * @param String $data
+     */ 
+    public function __construct($data = null) {
+        $this->populate($data);
+    }
 
-	public function __construct($data=null) {
-		$this->populate($data);
-	}
+    /**
+     * @param String $data
+     */ 
+    public function populate($data) {
+        if (isset($data->id)) {
+            $this->setId($data->id);
+        }
+        if (isset($data->name)) {
+            $this->setName($data->name);
+        }
+        if (isset($data->subcategory)) {
+            $this->setSubcategory(new Subcategory($data->subcategory));
+        }
+    }
 
-	public function populate($data) {
-		if (isset($data->id)) {
-			$this->setId($data->id);
-		}
-		if (isset($data->name)) {
-			$this->setName($data->name);
-		}
-		if (isset($data->subcategory)) {
-			$this->setSubcategory(new Subcategory($data->subcategory));
-		}
-	}
+    /**
+     * @return String
+     */ 
+    public function __toString() {
+        $category = $this->getName();
+        $category .= $this->getSubcategory() ? ' - ' . $this->getSubcategory() : '';
+        return $category;
+    }
 
-	public function __toString() {
-		$category = $this->getName();
-		$category .= $this->getSubcategory() ? ' - ' . $this->getSubcategory() : '';
-		return $category;
-	}
+    /**
+     * @param integer $id
+     */ 
+    public function setId($id) {
+        $this->id = $id;
+    }
 
-	public function setId($id) {
-		$this->id = $id;
-	}
+    /**
+     * @return integer
+     */ 
+    public function getId() {
+        return $this->id;
+    }
 
-	public function getId() {
-		return $this->id;
-	}
+    /**
+     * @param string $name
+     */ 
+    public function setName($name) {
+        $this->name = Inflector::formatTitle($name);
+    }
 
-	public function setName($name) {
-		$this->name = Inflector::formatTitle($name);
-	}
+    /**
+     * @return string
+     */ 
+    public function getName() {
+        return $this->name;
+    }
 
-	public function getName() {
-		return $this->name;
-	}
+    /**
+     * @param Subcategory $subcategory
+     */ 
+    public function setSubcategory($subcategory) {
+        $this->subcategory = $subcategory;
+    }
 
-	public function setSubcategory($subcategory) {
-		$this->subcategory = $subcategory;
-	}
-
-	public function getSubcategory() {
-		return $this->subcategory;
-	}
-
+    /**
+     * @return Subcategory
+     */ 
+    public function getSubcategory() {
+        return $this->subcategory;
+    }
 }
