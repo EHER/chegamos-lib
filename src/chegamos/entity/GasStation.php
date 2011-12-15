c8676faa5f302f9696e10468f81f65c010d24505
Eher <alexandre.eher@apontador.com>
2011-12-15 09:12:04 -0200

    Bobeiras de formatação.

--- src/chegamos/entity/GasStation.php
+++ src/chegamos/entity/GasStation.php
@@ -6,18 +6,18 @@ use chegamos\util\ItemsList;
 
 class GasStation extends ItemsList {
 
-	public function __construct($data=null) {
-		$this->populate($data);
-	}
+    public function __construct($data=null) {
+        $this->populate($data);
+    }
 
-	public function populate($data) {
-		if (isset($data) && is_array(new GasStation)) {
-			foreach ($data as $itemName => $value) {
-				if (strstr($itemName, 'price_') !== false) {
-					$this->add(GasStationItemFactory::generate($itemName, $data));
-				}
-			}
-		}
-	}
+    public function populate($data) {
+        if (isset($data) && is_array(new GasStation)) {
+            foreach ($data as $itemName => $value) {
+                if (strstr($itemName, 'price_') !== false) {
+                    $this->add(GasStationItemFactory::generate($itemName, $data));
+                }
+            }
+        }
+    }
 
 }
