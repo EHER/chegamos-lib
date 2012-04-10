b2a407bdf98ab4489f659b6ffd226abdae2713b1
Eher <alexandre.eher@apontador.com>
2012-04-10 15:31:11 -0300

    Generate signature base string

--- src/chegamos/rest/auth/OAuth.php
+++ src/chegamos/rest/auth/OAuth.php
@@ -34,4 +34,26 @@ class OAuth
     {
         return $accessToken;
     }
+
+    public function generateSignatureBaseString($verb, $url, $params)
+    {
+        ksort($params);
+        $baseString  = strtoupper($verb);
+        $baseString .= "&";
+        $baseString .= rawurlencode($url);
+        $baseString .= "&";
+        $baseString .= $this->http_build_query_rfc3986($params);
+
+        return $baseString;
+    }
+
+    private function http_build_query_rfc3986($queryData)
+    {
+        $queryArray = array();
+        foreach($queryData as $key => $value) {
+            $queryArray[] = $key . "=" . $value;
+        }
+        $queryString = join('&', $queryArray);
+        return rawurlencode($queryString);
+    }
 }
