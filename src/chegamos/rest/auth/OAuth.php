8d97a81e2f6c3ec07c668928e7c7aeeb206f576e
Eher <alexandre@eher.com.br>
2012-05-20 12:54:24 -0300

    Uma tapa no OAuth

--- src/chegamos/rest/auth/OAuth.php
+++ src/chegamos/rest/auth/OAuth.php
@@ -3,12 +3,13 @@
 namespace chegamos\rest\auth;
 
 use Eher\OAuth\Consumer;
+use Eher\OAuth\Token;
 
 class OAuth
 {
     private $consumer;
     private $requestToken;
-    private $accessToken;
+    private $token;
 
     public function __construct($key, $secret)
     {
@@ -25,20 +26,22 @@ class OAuth
         return $this->consumer;
     }
 
-    public function setAccessToken(AccessToken $accessToken)
+    public function setToken(Token $token)
     {
-        $this->AccessToken = $accessToken;
+        $this->token = $token;
     }
 
-    public function getAccessToken()
+    public function getToken()
     {
-        return $accessToken;
+        return $token;
     }
 
     public function generateSignature($verb, $url, $params)
     {
         $baseString = $this->generateSignatureBaseString($verb, $url, $params);
-        return  hash_hmac('sha1', $baseString, $this->consumer->key);
+        $signatureKey = $this->generateSignatureKey();
+        $octetString = hash_hmac('sha1', $baseString, $signatureKey, true);
+        return base64_encode($octetString);
     }
 
     public function generateSignatureBaseString($verb, $url, $params)
@@ -46,11 +49,38 @@ class OAuth
         ksort($params);
         $baseString  = strtoupper($verb);
         $baseString .= "&";
-        $baseString .= rawurlencode($url);
+        $baseString .= $this->urlencode_rfc3986($url);
         $baseString .= "&";
         $baseString .= $this->http_build_query_rfc3986($params);
 
         return $baseString;
+    }
+
+    public function generateSignatureKey()
+    {
+        $keyParts = array(
+            $this->consumer->secret,
+            ($this->token) ? $this->token->secret : ""
+        );
+
+        $keyParts = $this->urlencode_rfc3986($keyParts);
+        $signatureKey = implode('&', $keyParts);
+
+        return $signatureKey;
+    }
+
+    public function urlencode_rfc3986($input) {
+        if (is_array($input)) {
+            return array_map(array('Chegamos\rest\auth\OAuth', 'urlencode_rfc3986'), $input);
+        } else if (is_scalar($input)) {
+            return str_replace(
+                '+',
+                ' ',
+                str_replace('%7E', '~', rawurlencode($input))
+            );
+        } else {
+            return '';
+        }
     }
 
     private function http_build_query_rfc3986($queryData)
@@ -60,6 +90,6 @@ class OAuth
             $queryArray[] = $key . "=" . $value;
         }
         $queryString = join('&', $queryArray);
-        return rawurlencode($queryString);
+        return $this->urlencode_rfc3986($queryString);
     }
 }
--- tests/chegamos/rest/auth/OAuthTest.php
+++ tests/chegamos/rest/auth/OAuthTest.php
@@ -2,13 +2,16 @@
 
 namespace chegamos\rest\auth;
 
+use Eher\OAuth\Token;
+
 class OAuthTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGenerateSignatreBaseString()
+    public function testGenerateSignatureBaseString()
     {
         $oAuth = new OAuth("Key", "Secret");
+        $oAuth->setToken(new Token("Token", "TokenSecret"));
 
-        $expectedSignatureBaseString = "GET&http%3A%2F%2Fphotos.example.net%2Fphotos&file%3Dvacation.jpg%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3Dnnch734d00sl2jdk%26oauth_version%3D1.0%26size%3Doriginal";
+        $expectedSignatureBaseString = "GET&http%3A%2F%2Fphotos.example.net%2Fphotos&file%3Dvacation.jpg%26oauth_consumer_key%3DKey%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3DToken%26oauth_version%3D1.0%26size%3Doriginal";
         $this->assertEquals(
             $expectedSignatureBaseString,
             $oAuth->generateSignatureBaseString(
@@ -16,7 +19,32 @@ class OAuthTest extends \PHPUnit_Framework_TestCase
                 "http://photos.example.net/photos",
                 array(
                     "file" => "vacation.jpg",
-                    "oauth_consumer_key" => "dpf43f3p2l4k3l03",
+                    "oauth_consumer_key" => "Key",
+                    "oauth_nonce" => "kllo9940pd9333jh",
+                    "oauth_signature_method" => "HMAC-SHA1",
+                    "oauth_timestamp" => "1191242096",
+                    "oauth_token" => "Token",
+                    "oauth_version" => "1.0",
+                    "size" => "original",
+                )
+            )
+        );
+    }
+
+    public function testGenerateSignatureKey()
+    {
+        $oAuth = new OAuth("Key", "Secret");
+        $oAuth->setToken(new Token("Token", "TokenSecret"));
+
+        $expectedSignatureKey = "Secret&TokenSecret";
+        $this->assertEquals(
+            $expectedSignatureKey,
+            $oAuth->generateSignatureKey(
+                "GET",
+                "http://photos.example.net/photos",
+                array(
+                    "file" => "vacation.jpg",
+                    "oauth_consumer_key" => "Key",
                     "oauth_nonce" => "kllo9940pd9333jh",
                     "oauth_signature_method" => "HMAC-SHA1",
                     "oauth_timestamp" => "1191242096",
@@ -31,8 +59,9 @@ class OAuthTest extends \PHPUnit_Framework_TestCase
     public function testGenerateSignature()
     {
         $oAuth = new OAuth("Key", "Secret");
+        $oAuth->setToken(new Token("Token", "TokenSecret"));
 
-        $expectedSignature = "";
+        $expectedSignature = "6xiP+J+zZAaotb4rpblXKxrHUy4=";
         $this->assertEquals(
             $expectedSignature,
             $oAuth->generateSignature(
@@ -40,7 +69,7 @@ class OAuthTest extends \PHPUnit_Framework_TestCase
                 "http://photos.example.net/photos",
                 array(
                     "file" => "vacation.jpg",
-                    "oauth_consumer_key" => "dpf43f3p2l4k3l03",
+                    "oauth_consumer_key" => "Key",
                     "oauth_nonce" => "kllo9940pd9333jh",
                     "oauth_signature_method" => "HMAC-SHA1",
                     "oauth_timestamp" => "1191242096",
@@ -51,7 +80,6 @@ class OAuthTest extends \PHPUnit_Framework_TestCase
             )
         );
     }
-
     public function testGetHeader()
     {
         $oAuth = new OAuth("Key", "Secret");
