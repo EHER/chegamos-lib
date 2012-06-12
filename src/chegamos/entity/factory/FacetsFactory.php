aa4aa3d869e0298d6246fa9fc8cc2a1df22d9837
Rodrigo Prestes <rodrigo.prestes@apontador.com>
2012-06-12 15:30:03 -0300

    adiciona as classes de facet

---
+++ src/chegamos/entity/factory/FacetsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace chegamos\entity\factory;
+
+use chegamos\exception\ChegamosException;
+use chegamos\entity\Facet;
+
+class FacetsFactory
+{
+    public static function generate($jsonObject)
+    {
+        if (is_array($jsonObject)) {
+            $facets = array();
+            
+            foreach($jsonObject as $facetObject) {
+            	$facet = new Facet($facetObject->name);
+            	foreach ($facetObject->data as $data) {
+            		$facet->add($data->name, $data->count);
+            	}
+            	$facets[] = $facet;
+            }
+            return $facets;
+        } else {
+            throw new ChegamosException("Parâmetro inválido.");
+        }
+    }
+}
