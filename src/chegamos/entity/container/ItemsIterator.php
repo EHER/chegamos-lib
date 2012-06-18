6b36f3098008cbc93fd9c63195d619d4ab5baeca
Rodrigo Prestes <rodrigo.prestes@apontador.com>
2012-06-18 16:58:53 -0300

    corrige endentação

--- src/chegamos/entity/container/ItemsIterator.php
+++ src/chegamos/entity/container/ItemsIterator.php
@@ -4,42 +4,42 @@ namespace chegamos\entity\container;
 
 class ItemsIterator implements \Iterator, \Countable
 {
-	protected $items = array();
+    protected $items = array();
 
-	public function __construct(ItemsList $list)
-	{
-		$this->items = $list->getItems();
-	}
+    public function __construct(ItemsList $list)
+    {
+        $this->items = $list->getItems();
+    }
 
-	public function rewind()
-	{
-		reset($this->items);
-	}
+    public function rewind()
+    {
+        reset($this->items);
+    }
 
-	public function current()
-	{
-		return current($this->items);
-	}
+    public function current()
+    {
+        return current($this->items);
+    }
 
-	public function key()
-	{
-		return key($this->items);
-	}
+    public function key()
+    {
+        return key($this->items);
+    }
 
-	public function valid()
-	{
-		return $this->current() !== false;
-	}
+    public function valid()
+    {
+        return $this->current() !== false;
+    }
 
-	public function next()
-	{
-		return next($this->items);
-	}
+    public function next()
+    {
+        return next($this->items);
+    }
 
-	public function count()
-	{
-		return count($this->items);
-	}
+    public function count()
+    {
+        return count($this->items);
+    }
 
 
 }
